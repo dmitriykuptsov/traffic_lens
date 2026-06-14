e651ddb47f54bee8530c427d78ca2ac905e58176
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2026-06-14 13:34:22 +0500

    Added ClickHouse schema

--- infrastructure/clickhouse/schema.sql
+++ infrastructure/clickhouse/schema.sql
@@ -1,7 +1,26 @@
-CREATE TABLE metrics_raw (
- ts DateTime64(3),
- device_id String,
- metric String,
- value Float64
-) ENGINE=MergeTree
-ORDER BY (device_id, metric, ts);
+CREATE TABLE flow_records
+(
+    ts DateTime64(3),
+
+    exporter_ip String,
+
+    src_ip IPv4,
+    dst_ip IPv4,
+
+    src_port UInt16,
+    dst_port UInt16,
+
+    protocol UInt8,
+
+    bytes UInt64,
+    packets UInt64,
+
+    input_if UInt16,
+    output_if UInt16,
+
+    src_as UInt32,
+    dst_as UInt32
+)
+ENGINE = MergeTree
+PARTITION BY toDate(ts)
+ORDER BY (ts, exporter_ip);
